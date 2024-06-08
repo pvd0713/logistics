d303596a58490c2d984f13fab0bca5fc145c8049
Pavel <pvdanilov88@inbox.ru>
2024-06-08 19:00:01 +0300

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  puplicPath: `/logistics/`,
   plugins: [
     vue(),
-    vueJsx(),
+    vueJsx()   
     // VueDevTools(),
   ],
   resolve: {
